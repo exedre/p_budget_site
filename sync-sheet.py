1dcf7f2655522af66943bc21fd869a209f892c37
Emmanuele Somma <exedre@gmail.com>
2021-01-01 22:39:08 +0100

    step

--- sync-sheet.py
+++ sync-sheet.py
@@ -153,6 +153,8 @@ def main(debug,debug_section):
     values = setup_bilancio(values,'CE_RICAVI',descrizioni)
     values = setup_bilancio(values,'SP_ATTIVITA',descrizioni)
     values = setup_bilancio(values,'SP_PASSIVITA',descrizioni)
+    debiti = values['A_CRED'] - values['R_CRED']
+    values['P_DEB']=debiti
     utile_o_perdita = values['R_T'] - values['C_T']
     if utile_o_perdita > 0:
         # Utile
